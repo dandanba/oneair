0b9c361cc915f8f8826b76487e4bfe6db67ddc2a
王 庚 <13641194007@139.com>
2015-07-13 12:45:27 +0800

    增加 cloud 和 bluetooth两个版本

--- oneair.cleanfu/src/com/oneair/Constants.java
+++ oneair.cleanfu/src/com/oneair/Constants.java
@@ -1,11 +1,13 @@
 package com.oneair;
+
 import java.io.File;
 import android.os.Environment;
 
 public class Constants {
-	public static boolean CLOUD_READ = true; // 从云端读取还是蓝牙读取
+	public static boolean CLOUD_READ = false; // 从云端读取还是蓝牙读取
 	public static String PACKAGE_NAME = "com.oneair.cleanfu"; // 程序的包名
-	public static String UMENG_KEY = "559e386d67e58efc3d00079c"; // 对应 PACKAGE_NAME 的有盟key
+	public static String UMENG_CLOUD_KEY = "55a33cc967e58e7c69001f46"; // 对应 PACKAGE_NAME 的有盟key com.oneair.cleanfu.cloud
+	public static String UMENG_BLUETOOTH_KEY = "55a33d7067e58e63d400071f"; // 对应 PACKAGE_NAME 的有盟key com.oneair.cleanfu.bluetooth
 	public final static String DEVICE_ADDRESS = "B4:99:4C:4C:D5:0B"; // 蓝牙的 device address
 	public final static String AVOS_APP_TAG = "cleanfu";
 	public static String UPDATE_ID = "559f45ebe4b0d6d68fe82811"; // 编辑的id
